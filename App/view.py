de2413d11c71a1f79f5089bc9cf5f647f7d17712
larubiano0 <larubiano0@gmail.com>
2020-12-15 09:40:36 -0500

    Final

--- App/view.py
+++ App/view.py
@@ -119,60 +119,60 @@ def optionFive():
     horario = controller.f5(cont, CAI, CAF, HI, HF)
 
 while True:
-    #try:
-    printMenu()
-    inputs = input('Seleccione una opción para continuar\n>')
+    try:
+        printMenu()
+        inputs = input('Seleccione una opción para continuar\n>')
 
-    if int(inputs) == 0:
-        print("\nHasta pronto!")
-        break
+        if int(inputs) == 0:
+            print("\nHasta pronto!")
+            break
 
-    if int(inputs) == 1:
-        executiontime = timeit.timeit(optionOne, number=1)
-        print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
+        if int(inputs) == 1:
+            executiontime = timeit.timeit(optionOne, number=1)
+            print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
 
-    elif int(inputs) == 2:
-        tamaño = input("Si desea trabajar con el archivo de datos grande escriba L, con el mediano M, y con el pequeño S: ").upper()
-        if tamaño=="L": tamaño = large
-        elif tamaño=="M": tamaño = medium
-        elif tamaño=="S": tamaño = small
-        executiontime = timeit.timeit(optionTwo, number=1)
-        print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
+        elif int(inputs) == 2:
+            tamaño = input("Si desea trabajar con el archivo de datos grande escriba L, con el mediano M, y con el pequeño S: ").upper()
+            if tamaño=="L": tamaño = large
+            elif tamaño=="M": tamaño = medium
+            elif tamaño=="S": tamaño = small
+            executiontime = timeit.timeit(optionTwo, number=1)
+            print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
 
-    elif int(inputs) == 3:
-        M = int(input("Ingrese el número de compañías que quiere que se muestren por número de táxis afiliados: "))
-        N = int(input("Ingrese el número de compañías que quiere que se muestren por número de servicios prestados: "))
-        executiontime = timeit.timeit(optionThree, number=1)
-        print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
+        elif int(inputs) == 3:
+            M = int(input("Ingrese el número de compañías que quiere que se muestren por número de táxis afiliados: "))
+            N = int(input("Ingrese el número de compañías que quiere que se muestren por número de servicios prestados: "))
+            executiontime = timeit.timeit(optionThree, number=1)
+            print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
 
-    elif int(inputs) == 4:
-        print("¿Desea obtener el top de taxis usando la función alfa, para un solo día o para un rango de fechas?")
-        booleano = int(input("""Ingrese "1" para un solo día o "2" para un rango de fechas: """))
-        Ñ = int(input("Ingrese el número de táxis que quiere que se muestren en el top: "))
-        if booleano==1:
-            FechaI = input("Fecha (YYYY-MM-DD): ")
-            FechaI = datetime.datetime.strptime(FechaI, '%Y-%m-%d').date()
-        elif booleano==2:
-            FechaI = input("Fecha inicial (YYYY-MM-DD): ")
-            FechaI = datetime.datetime.strptime(FechaI, '%Y-%m-%d').date()
-            FechaF = input("Fecha final (YYYY-MM-DD): ")
-            FechaF = datetime.datetime.strptime(FechaF, '%Y-%m-%d').date()
-        executiontime = timeit.timeit(optionFour, number=1)
-        print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
-    
-    elif int(inputs) == 5:
-        CAI = str(float(input("Por favor introduzca el número de la Community Area de la cual quiere salir: ")))
-        CAF = str(float(input("Por favor introduzca el número de la Community Area a la cual quiere llegar: ")))
-        HI = input("Hora Inicial (HH:MM): ")
-        HI = "1900-01-01 " + HI
-        HF = input("Hora Final (HH:MM): ")
-        HF = "1900-01-01 " + HF
-        HI = datetime.datetime.strptime(HI, '%Y-%m-%d %H:%M').time()
-        HF = datetime.datetime.strptime(HF, '%Y-%m-%d %H:%M').time()
-        executiontime = timeit.timeit(optionFive, number=1)
-        print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
+        elif int(inputs) == 4:
+            print("¿Desea obtener el top de taxis usando la función alfa, para un solo día o para un rango de fechas?")
+            booleano = int(input("""Ingrese "1" para un solo día o "2" para un rango de fechas: """))
+            Ñ = int(input("Ingrese el número de táxis que quiere que se muestren en el top: "))
+            if booleano==1:
+                FechaI = input("Fecha (YYYY-MM-DD): ")
+                FechaI = datetime.datetime.strptime(FechaI, '%Y-%m-%d').date()
+            elif booleano==2:
+                FechaI = input("Fecha inicial (YYYY-MM-DD): ")
+                FechaI = datetime.datetime.strptime(FechaI, '%Y-%m-%d').date()
+                FechaF = input("Fecha final (YYYY-MM-DD): ")
+                FechaF = datetime.datetime.strptime(FechaF, '%Y-%m-%d').date()
+            executiontime = timeit.timeit(optionFour, number=1)
+            print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
+        
+        elif int(inputs) == 5:
+            CAI = str(float(input("Por favor introduzca el número de la Community Area de la cual quiere salir: ")))
+            CAF = str(float(input("Por favor introduzca el número de la Community Area a la cual quiere llegar: ")))
+            HI = input("Hora Inicial (HH:MM): ")
+            HI = "1900-01-01 " + HI
+            HF = input("Hora Final (HH:MM): ")
+            HF = "1900-01-01 " + HF
+            HI = datetime.datetime.strptime(HI, '%Y-%m-%d %H:%M').time()
+            HF = datetime.datetime.strptime(HF, '%Y-%m-%d %H:%M').time()
+            executiontime = timeit.timeit(optionFive, number=1)
+            print("Tiempo de ejecución: " + str(executiontime)+ " segundos")
 
-    #except:
-     #   print("\nAlgo ocurrió mal, asegurese que todo esté bien e intente nuevamente: ")
+    except:
+        print("\nAlgo ocurrió mal, asegurese que todo esté bien e intente nuevamente: ")
 
 sys.exit(0)
